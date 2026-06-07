b9c71dd852c0e0ba361f06ccba86d146a1590b8b
Correy Xu <cxu002@odu.edu>
2026-06-07 14:24:24 -0400

    update menu choices

--- src/nikke_cjjc_automator/view/menu.py
+++ src/nikke_cjjc_automator/view/menu.py
@@ -8,6 +8,13 @@ SELECT_MODE_CHOICES = [
     "2: Review Mode (Please enter the result page showing 5 teams in advance)",
     "3: Anti-Buy Mode (Please enter the betting page in advance)",
     "4: League Prediction Mode (Please enter the league page in advance)",
+    "5: 1v2 (View bracket)",
+    "6: 3v4 (View bracket)",
+    "7: 5v6 (View bracket)",
+    "8: 7v8 (View bracket)",
+    "9: 1&2 v 3&4 (View bracket)",
+    "10: 5&6 v 7&8 (View bracket)",
+    "11: 1&2&3&4 v 5&6&7&8 (View bracket)"
 ]
 
 def select_mode() -> Optional[int]:
